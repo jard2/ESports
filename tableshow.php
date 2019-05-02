1d6d4e0da23e69bf74022f09454db2718107b9f4
Nick Pappas <njpappas97@gmail.com>
2019-05-02 00:26:52 -0400

    added function to support my code

--- tableshow.php
+++ tableshow.php
@@ -218,4 +218,43 @@ function show_team_stats($conn) {
 	echo '</tbody>';
 	echo '</table>';
 }
+
+
+function show_members($conn){
+
+//include "dbconnect.php";
+
+$sql = "SELECT * FROM members";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+	echo "<br><h3> Members Table<h3> <br>";
+
+	echo '<table border>';
+	echo '<thead><tr>';
+	echo '<th>'."Player ID".'</th>'.'<th>'."Team ID";
+	echo '</tr></thead>';
+	echo '<tbody>';
+
+	while($row = $result->fetch_assoc()) {
+		echo '<tr>';
+				echo "<td>" . $row["player_ID"]. "</td>";
+        echo "<td>" . $row["team_ID"]. "</td>";
+
+		echo '</tr>';
+    }
+
+	echo '</tbody>';
+	echo '</table>';
+
+    // output data of each row
+
+
+} else {
+    echo "0 results";
+}
+//$conn->close();
+}
+
 ?>
